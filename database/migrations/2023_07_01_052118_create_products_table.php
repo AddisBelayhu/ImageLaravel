900dcc8f95d3e7dea7eb0b9c128111a61cba9a9f
AddisBelayhu <addisgithub@gmail.com>
2023-07-01 09:27:14 +0300

    second commit

--- database/migrations/2023_07_01_052118_create_products_table.php
+++ database/migrations/2023_07_01_052118_create_products_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProductTable extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('detail');
+            $table->string('image');
             $table->timestamps();
         });
     }
